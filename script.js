5bdaad1a60f9effef7c87c1db5d1d1c1b2eddb3f
Ludwig Angermund <Ludwig.angermund@elev.ga.ntig.se>
2024-02-21 13:22:36 +0100

    slut på lektion

---
+++ script.js
@@ -0,0 +1,20 @@
+let namn = document.querySelector(".namn");
+let pris =document.querySelector(".pris");
+let bild = document.querySelector(".articleImg");
+
+const array=[];
+
+function kundvagn(bild, pris, namn){
+    let produkt = {
+        namn: namn.value,
+        pris: pris.value,
+        bild: bild.value
+        }
+        console.log(array);
+
+        array.push
+
+
+
+}
+
