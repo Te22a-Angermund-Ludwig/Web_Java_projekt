591362c5bd084075468fff25329b78e2cbbddda1
Ludwig Angermund <Ludwig.angermund@elev.ga.ntig.se>
2024-03-03 16:07:41 +0100

    sparar objekt i local storage

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ let bild = document.querySelector(".articleImg");
 
 const array=[];
 
-function kundvagn(bild, pris, namn){
+function Travis_1_kundvagn(){
     let produkt = {
-        namn: namn.value,
-        pris: pris.value,
-        bild: bild.value
+        namn: "Travis 1 mocha",
+        pris: "2000",
+        bild: "img/cactus-jack-low1s.webp"
         }
-        console.log(array);
+        
 
-        array.push
-
-
-
+    console.log("namn: "+produkt.namn+", pris: "+produkt.pris+", bild: "+produkt.bild);
+    let produkt_fält=[];
+    let produkt_fält_Json=window.localStorage.getItem("produkt_fält");
+    if (produkt_fält_Json) {
+        produkt_fält = JSON.parse(produkt_fält_Json);
+    }
+    produkt_fält.push(produkt);
+    console.log("produkt fält: "+produkt_fält);
+    produkt_fält_Json=JSON.stringify(produkt_fält);
+    window.localStorage.setItem("produkt_fält", produkt_fält_Json);
+    
+    
 }
-
